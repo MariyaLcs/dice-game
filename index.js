88435cb247a64a7dd86aad89e9275ff8200be4f3
mariyalcs <mariya.podosinova@gmail.com>
2020-06-26 15:23:01 +0100

    message logic

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ let image2 = document.querySelectorAll("img")[1];
 
 image1.setAttribute("src", `images/dice${randomNumber1}.png`);
 image2.setAttribute("src", `images/dice${randomNumber2}.png`);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
